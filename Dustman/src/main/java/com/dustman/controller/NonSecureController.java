19449c4db27cd974f391fd2eb0b3aff837f9ad68
sourav6251 <dassourav3738+github@gmail.com>
2025-04-25 19:44:22 +0530

    comment security

--- Dustman/src/main/java/com/dustman/controller/NonSecureController.java
+++ Dustman/src/main/java/com/dustman/controller/NonSecureController.java
@@ -20,49 +20,51 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 //@RequestMapping("/api")
 public class NonSecureController {
-    @Autowired
-    private EmailService emailService;
-
-    private final UserService userService;
-    private final AuthenticationManager authenticationManager;
-    private final JwtUtil jwtUtil;
-    private final UserDetailsService userDetailsService;
-
-    public NonSecureController( UserDetailsService userDetailsService,UserService userService, AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
-        this.userDetailsService= userDetailsService;
-        this.userService = userService;
-        this.authenticationManager = authenticationManager;
-        this.jwtUtil = jwtUtil;
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity<Object> saveUser(@RequestBody UserDTO registerInfo) {
-        Status result = userService.registerUser(registerInfo);
-        return ResponseEntity.status(result.code()).body(result.data());
-    }
-
-    @PostMapping("/user_login")
-    public ResponseEntity<String> login(@RequestBody UserDTO loginInfo) {
-        String userName= loginInfo.getUsername();
-        String password= loginInfo.getPassword();
-        System.out.println("Enter into login");
-        try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userName,password));
-        } catch (BadCredentialsException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("username and Password is incorrect");
-        }
-
-        UserDetails userDetails= userDetailsService.loadUserByUsername(userName);
-        String jwt= jwtUtil.generateJWT(userDetails);
-
-        return ResponseEntity.ok(jwt);
-    }
-
-    @PatchMapping("/updaterole")
-    public ResponseEntity<String> updateRole(@RequestBody UserDTO updateRole) {
-        System.out.println(updateRole.getUserId());
-        return ResponseEntity.ok(userService.updateRole(updateRole));
-    }
+//    @Autowired
+//    private EmailService emailService;
+//
+//    private final UserService userService;
+//    private final AuthenticationManager authenticationManager;
+//    private final JwtUtil jwtUtil;
+//    private final UserDetailsService userDetailsService;
+//
+//    public NonSecureController( UserDetailsService userDetailsService,UserService userService, AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+//        this.userDetailsService= userDetailsService;
+//        this.userService = userService;
+//        this.authenticationManager = authenticationManager;
+//        this.jwtUtil = jwtUtil;
+//    }
+//
+//    @PostMapping("/register")
+//    public ResponseEntity<Object> saveUser(@RequestBody UserDTO registerInfo) {
+//        Status result = userService.registerUser(registerInfo);
+//        return ResponseEntity.status(result.code()).body(result.data());
+//    }
+//
+//    @PostMapping("/user_login")
+//    public ResponseEntity<String> login(@RequestBody UserDTO loginInfo) {
+//        String userName= loginInfo.getUsername();
+//        String password= loginInfo.getPassword();
+//        System.out.println("Enter into login");
+//        try {
+//            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userName,password));
+//        } catch (BadCredentialsException e) {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("username and Password is incorrect");
+//        }
+//
+//        UserDetails userDetails= userDetailsService.loadUserByUsername(userName);
+//        String jwt= jwtUtil.generateJWT(userDetails);
+//
+//        return ResponseEntity.ok(jwt);
+//    }
+//
+//    @PatchMapping("/updaterole")
+//    public ResponseEntity<String> updateRole(@RequestBody UserDTO updateRole) {
+//        System.out.println(updateRole.getUserId());
+//        return ResponseEntity.ok(userService.updateRole(updateRole));
+//    }
+//
+//
 
 }
 
