4656225ba544098411527483ffabcbb00e155699
shuvra.patra <shuvra.patra@definedge.com>
2025-04-29 21:19:11 +0530

    changes done form UI side

--- Dustman-frontend/src/router/index.ts
+++ Dustman-frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LandingPage from '@/views/LandingPage.vue'
+import HomeVieww from '@/views/HomeVieww.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/home',
       name: 'HomePage',
       component: HomeView,
+    },
+    {
+      path: '/homee',
+      name: 'HomePagee',
+      component: HomeVieww,
     },
     // {
     //   path: '/about',
