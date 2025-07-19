350df43a5099d7fc6a62f8dd28aabb7202393723
Sukanta191202 <sukantadua2002@gmail.com>
2025-07-20 02:02:06 +0530

    Add Home Page

---
+++ Dustman-frontend/src/components/common/homePage.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-row class="pa-10" justify="center" style="min-height: 100vh;">
+
+ 
+    <v-col cols="12" md="3">
+        <v-img
+        src="https://images.unsplash.com/photo-1543508282-6319a3e2621f?q=80&w=2030&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        height="200px"
+        cover
+        ></v-img>
+        <v-card>
+            <v-row class="pa-2">
+            <v-card-title>Shoes 2</v-card-title>
+            <v-spacer></v-spacer>
+            <v-card-title> 
+                <v-avatar size="14" color="green"></v-avatar>
+            </v-card-title>
+            </v-row>
+          <v-card-item>
+            <v-card-subtitle>Nederland</v-card-subtitle>
+          </v-card-item>
+        </v-card>
+    </v-col>
+
+    <!-- Using Markup Only 1 -->
+    <v-col cols="12" md="3">
+      <v-img
+        src="https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        height="200px"
+        cover
+      ></v-img>
+      <v-card>
+        <v-card-item>
+          <v-card-title>Shoes 3</v-card-title>
+          <v-card-subtitle>Charcoal</v-card-subtitle>
+        </v-card-item>
+      </v-card>
+    </v-col>
+
+    <!-- Using Markup Only 2 -->
+    <v-col cols="12" md="3">
+      <v-card>
+        <v-img
+          src="https://images.unsplash.com/photo-1603808033192-082d6919d3e1?q=80&w=2030&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          height="200px"
+          cover
+        ></v-img>
+
+        <v-card-item>
+          <v-card-title>Shoes 4</v-card-title>
+          <v-card-subtitle>Iso Dots</v-card-subtitle>
+        </v-card-item>
+      </v-card>
+    </v-col>
+
+  </v-row>
+</template>
